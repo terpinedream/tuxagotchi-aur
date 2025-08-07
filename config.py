bf6a6cc6c7bd69aec56305b7595e4dd5e83e542e
terpinedream <brenden.panageas@gmail.com>
2025-08-07 09:06:35 -0400

    Fixed pathing for config.toml

--- config.py
+++ config.py
@@ -1,8 +1,18 @@
+import os
 import toml
 
 
 def load_config():
-    config = toml.load("config.toml")
+    user_config_path = os.path.expanduser("~/.config/tuxagotchi/config.toml")
+
+    default_config_path = os.path.join(os.path.dirname(__file__), "config.toml")
+
+    config_path = (
+        user_config_path if os.path.exists(user_config_path) else default_config_path
+    )
+
+    config = toml.load(config_path)
+
     # Provide fallback defaults for colors
     colors = config.get("colors", {})
     config["colors"] = {
